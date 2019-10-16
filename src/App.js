0dae8d817d92efb3e163a32c255de7ea0891636f
karan413255 <karanshah413255@gmail.com>
2019-10-16 11:41:42 +0530

    Added search, artist and album pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import SearchSpotify from "./components/SearchSpotify";
+import Artist from "./components/Artist";
+import Album from "./components/Album";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React, its good!
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React, its good!
-        </a>
-      </header>
+    <div className="search-box">
+      <div>
+        <Link to="/search">Search</Link>
+      </div>
     </div>
   );
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <Route exact path="/" render={routeProps => <Home />} />
+          <Route
+            path="/search"
+            render={routeProps => <SearchSpotify {...routeProps} />}
+          />
+          <Route
+            path="/artist/:id"
+            render={routeProps => <Artist {...routeProps} />}
+          />
+          <Route
+            path="/album/:id"
+            render={routeProps => <Album {...routeProps} />}
+          />
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Artist.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Artist extends Component {
+  constructor(props) {
+    super(props);
+  }
+}
+
+export default Artist;
