092eb44683a1b583ec212f38284d96e97e847756
karan413255 <karanshah413255@gmail.com>
2019-10-17 03:03:27 +0530

    updated package,json

--- src/components/SearchSpotify.js
+++ src/components/SearchSpotify.js
@@ -4,20 +4,19 @@ class SearchSpotify extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchValue = "",
-      artistList = [],
-      albumList = [],
-      songsList = []
-    }
+      searchValue: "",
+      artistList: [],
+      albumList: [],
+      songsList: []
+    };
   }
-  
+
   componentDidMount() {
     // api call to get the search term details
   }
 
   render() {
-    return <div>
-    </div>;
+    return <div></div>;
   }
 }
 
