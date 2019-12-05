24c914dabf0433c8903722aee73a4a8a5de68075
karan413255 <karanshah413255@gmail.com>
2019-12-06 02:20:14 +0530

    artist detail page

--- src/views/Artist.js
+++ src/views/Artist.js
@@ -1,16 +1,44 @@
 import React, { Component } from "react";
+import artist from "../constants/artist";
+import Album from "../components/album";
 
 class Artist extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      albums: []
+    };
+  }
+
+  componentDidMount() {
+    const albums = artist.items;
+    console.log(albums);
+    this.setState({ albums });
   }
 
   render() {
+    const { albums } = this.state;
+    if (albums === null) {
+      return "Loading...";
+    }
     return (
-      <div></div>
+      <div className="artistPage">
+        <div className="artist-navbar">
+          {/* <div className="artist-name">{album.name}</div> */}
+        </div>
+        <div className="artist-main">
+          <div className="artist-info">
+            <div className="artist-image">
+              {/* <img src={artist.images[1].url} alt={artist.name}></img> */}
+            </div>
+            <div className="artist-albums">
+              {albums ? albums.map(album => <Album album={album} />) : null}
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
-
 
 export default Artist;
